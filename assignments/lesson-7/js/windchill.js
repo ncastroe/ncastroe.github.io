602242cb63453598e9686e74345c71fe7a207e3c
ncastroe <38932968+ncastroe@users.noreply.github.com>
2018-06-16 18:03:32 -0500

    week 8

---
+++ assignments/lesson-7/js/windchill.js
@@ -0,0 +1,31 @@
+function windChill(t, s) {
+    var d = new Date();
+    
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    
+    
+    var monthName = new Array(12);
+    monthName[0] = "January";
+    monthName[1] = "February";
+    monthName[2] = "March";
+    monthName[3] = "April";
+    monthName[4] = "May";
+    monthName[5] = "June";
+    monthName[6] = "July";
+    monthName[7] = "August";
+    monthName[8] = "September";
+    monthName[9] = "October";
+    monthName[10] = "November";
+    monthName[11] = "December";
+    
+
+    var dayName = weekday[d.getDay()];
+    document.getElementById("currentdate").innerHTML = dayName + ", " + d.getDate() + " " + monthName[d.getMonth()] + " " + d.getFullYear();
+}
